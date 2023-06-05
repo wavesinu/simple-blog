6a4979f8561638175f805e9b6d8a2e029ae57faa
ISU <wavesinu@gmail.com>
2023-06-05 11:47:35 +0900

    Add: HomeController 추가
    Thymeleaf 템플릿을 연결하는 HomeController 구현

---
+++ spring-boot-blog-application/src/main/java/com/example/springbootblogapplication/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.example.springbootblogapplication.controller;
+
+import com.example.springbootblogapplication.model.Post;
+import com.example.springbootblogapplication.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+    @Autowired
+    private PostService postService;
+
+    @GetMapping("/")
+    public String home(Model model) {
+        List<Post> posts = postService.getAll();
+        model.addAttribute("posts", posts);
+        return "home";
+    }
+}
---
+++ spring-boot-blog-application/src/main/java/com/example/springbootblogapplication/controller/PostController.java
@@ -0,0 +1,4 @@
+package com.example.springbootblogapplication.controller;
+
+public class PostController {
+}
