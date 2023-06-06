60e42e058a382ffd41f7c9fd23aeb72f3c5dd049
ISU <wavesinu@gmail.com>
2023-06-06 16:49:16 +0900

    Add: PostController 추가
    게시물을 관리하기 위한 PostController 클래스 정의

--- spring-boot-blog-application/src/main/java/com/example/springbootblogapplication/controller/PostController.java
+++ spring-boot-blog-application/src/main/java/com/example/springbootblogapplication/controller/PostController.java
@@ -1,4 +1,31 @@
 package com.example.springbootblogapplication.controller;
 
+import com.example.springbootblogapplication.model.Post;
+import com.example.springbootblogapplication.service.PostService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
+
+@Controller
 public class PostController {
+    private final PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @GetMapping("/posts/{id}")
+    public String getPost(@PathVariable Long id, Model model) {
+        Optional<Post> optionalPost = postService.getById(id);
+        if (optionalPost.isPresent()) {
+            Post post = optionalPost.get();
+            model.addAttribute("post", post);
+            return "post";
+        } else {
+            return "404";
+        }
+    }
 }
